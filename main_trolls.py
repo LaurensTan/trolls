81f204fece519fd4b55d661c0210a9f284fdd723
Laurens Tan <laurens.tan@itvitaelearning.nl>
2019-02-25 14:34:54 +0100

    datetime casting

--- main_trolls.py
+++ main_trolls.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # coding: utf-8
 
 from datetime import datetime 
@@ -9,31 +8,37 @@ import pandas as pd
 import re
 
 
+def clean_data(df):
+    cols = ['publish_date', 'harvested_date']
+    df[cols] = df[cols].apply(pd.to_datetime, format='%m/%d/%Y %H:%M')
+
+
 def main():
     folder = os.path.join(os.getcwd(), 'data')
     file = os.path.join(folder, 'IRAhandle_tweets_1.csv')
-    print(file)
     df = pd.read_csv(file)
-
-
-    # info:
-    # 
-    # region: Regio waar de tweet vandaan komt (?)
-    # 
-    # author: Accountname
-    # 
-    # language: taal van de tweet
-    # 
-    # retweet: 0 of 1, dus is het een retweet of niet
-    # account_category: RightTroll', 'NonEnglish', 'Fearmonger', 'LeftTroll', 'Unknown', 'HashtagGamer', 'NewsFeed', 'Commercial'
-    # 
-    # Updates: aantal twitter berichten gepost?
-    # 
-
-
-    df[df['language'] == 'Russian']
-    print(df)
+    
+    clean_data(df)
+    
+    #df[df['language'] == 'Russian']
+    print(df.head())
 
 
 if __name__ == '__main__':
     main()
+
+
+"""
+info:
+
+region: Regio waar de tweet vandaan komt (?)
+
+author: Accountname
+
+language: taal van de tweet
+
+retweet: 0 of 1, dus is het een retweet of niet
+account_category: RightTroll', 'NonEnglish', 'Fearmonger', 'LeftTroll', 'Unknown', 'HashtagGamer', 'NewsFeed', 'Commercial'
+
+Updates: aantal twitter berichten gepost?
+"""
